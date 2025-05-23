05681f774ca6ba9a8ca5bc1af508ea3394be3849
Vörös Bence <100135396+VeryOriginalIK@users.noreply.github.com>
2025-05-23 15:40:41 +0200

    Update Stock.js

--- Server/Stock.js
+++ Server/Stock.js
@@ -1,84 +1,92 @@
-export default class Stock
-{
-    static _id = 0;
-    
-    constructor(name, risk, mult, max_count, baseprice, changeRate)
-    {
-        this.ChangeRate = changeRate/100;
-        this.CostData = [];
-        this.CostData.push(baseprice);
-        this.MaxCount = max_count;
-        this.Count = max_count;
-        this.Name = name;
-        this.Id = ++Stock._id;
-        this.Mult = mult;
-        this.Risk = risk;
+export default class Stock {
+  static _id = 0;
+
+  constructor(name, risk, mult, max_count, baseprice, changeRate) {
+    this.ChangeRate = changeRate / 100;
+    this.CostData = [];
+    this.CostData.push(baseprice);
+    this.MaxCount = max_count;
+    this.Count = max_count;
+    this.Name = name;
+    this.Id = ++Stock._id;
+    this.Mult = mult;
+    this.Risk = risk;
+  }
+
+  Step() {
+    const currentPrice = this.CostData[this.CostData.length - 1];
+
+    const shortTermFluctuation = this.getRandNumBetween(-this.Risk, this.Risk);
+
+    const longTermTrend = this.Mult * 0.1;
+
+    let newPrice = currentPrice + shortTermFluctuation + longTermTrend;
+
+    newPrice = Math.max(newPrice, 0);
+
+    this.CostData.push(newPrice);
+  }
+
+  Buy(amount) {
+    const currentPrice = this.CostData[this.CostData.length - 1];
+    const newPrice = currentPrice;
+    const priceIncrease = 0;
+
+    for (let i = 0; i < amount; i++) {
+      if (this.Count <= 0) {
+        console.log("No more stocks available to buy.");
+        return false;
+      }
+      this.Count -= 1;
+
+      priceIncrease = currentPrice * this.ChangeRate;
+      newPrice = currentPrice + priceIncrease;
     }
-    
-    Step() {
-        const currentPrice = this.CostData[this.CostData.length - 1];
-
-        const shortTermFluctuation = this.getRandNumBetween(-this.Risk, this.Risk);
-        
-        const longTermTrend = this.Mult * 0.1; 
-
-        let newPrice = currentPrice + shortTermFluctuation + longTermTrend;
-
-        newPrice = Math.max(newPrice, 0);
-
-        this.CostData.push(newPrice);
-
+    if (newPrice != currentPrice) {
+      this.CostData.push(newPrice);
     }
-    
-    Buy() 
-    {
-        if (this.Count <= 0) {
-            console.log("No more stocks available to buy.");
-            return;
-        }
+    return true;
+  }
 
-        const currentPrice = this.CostData[this.CostData.length - 1];
-
-        this.Count -= 1;
-
-        const priceIncrease = currentPrice * this.ChangeRate;  
-        const newPrice = currentPrice + priceIncrease;
-        this.CostData.push(newPrice); 
-    }
-    Sell() {
-        if (this.Count >= this.MaxCount) {
-            console.log("You cannot sell stocks you do not own.");
-            return;
-        }
-
-        const currentPrice = this.CostData[this.CostData.length - 1];
-
-        this.Count += 1;
-
-        const priceDecrease = currentPrice * this.ChangeRate;
-        const newPrice = currentPrice - priceDecrease;
-        this.CostData.push(newPrice); 
-    }
-    GetCurrentPrice() 
-    {
-        return this.CostData.length > 0 ? this.CostData[this.CostData.length - 1] : 100;
-    }   
-    getRandNumBetween(start, end)
-    {
-        return Math.random() * (end - start) + start;
-    }
-    GetStockObject()
-    {
-        return {
-            "Name": this.Name,
-            "Id": this.Id,
-            "CostData": this.CostData,
-            "Risk": this.Risk,
-            "Mult": this.Mult,
-            "ChangeRate": this.ChangeRate,
-            "MaxCount": this.MaxCount,
-            "Count": this.Count
-        };
+  Sell(amount) {
+    const currentPrice = this.CostData[this.CostData.length - 1];
+    const priceDecrease = 0;
+    let newPrice = currentPrice;
+    for (let i = 0; i < amount; i++) {
+      if (this.Count >= this.MaxCount) {
+        console.log("You cannot sell stocks you do not own.");
+        return false;
+      }
+      priceDecrease = currentPrice * this.ChangeRate;
+      newPrice = currentPrice - priceDecrease;
     }
 
+    this.Count += amount;
+    if (newPrice != currentPrice) {
+      this.CostData.push(newPrice);
+    }
+    return true;
+  }
+
+  GetCurrentPrice() {
+    return this.CostData.length > 0
+      ? this.CostData[this.CostData.length - 1]
+      : 100;
+  }
+  
+  getRandNumBetween(start, end) {
+    return Math.random() * (end - start) + start;
+  }
+  GetStockObject() {
+    return {
+      Name: this.Name,
+      Id: this.Id,
+      CostData: this.CostData,
+      Risk: this.Risk,
+      Mult: this.Mult,
+      ChangeRate: this.ChangeRate,
+      MaxCount: this.MaxCount,
+      Count: this.Count,
+    };
+  }
 }
